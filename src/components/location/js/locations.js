c4deb835634f102cf35c19d2e3f62c24b5438999
Shivam Kumar <shivam.kumar@gdn-commerce.com>
2020-09-26 01:02:40 +0530

    added loading

--- src/components/location/js/locations.js
+++ src/components/location/js/locations.js
@@ -2,20 +2,35 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'locations',
   created () {
-    this.fetchLocations()
+    this.getLocations()
+  },
+  data () {
+    return {
+      loading: false
+    }
   },
   computed: {
     ...mapGetters('locations', ['availableLocations', 'locationsInfo'])
   },
   methods: {
     ...mapActions('locations', ['fetchLocations']),
+    async getLocations (configs) {
+      try {
+        this.loading = true
+        await this.fetchLocations(configs)
+      } catch (error) {
+        this.$message(error)
+      } finally {
+        this.loading = false
+      }
+    },
     changeLocationsPage (pageNo) {
       const configs = {
         params: {
           page: pageNo
         }
       }
-      this.fetchLocations(configs)
+      this.getLocations(configs)
     },
     handleLocationClick (row) {
       this.$emit('on-location-select', row)
