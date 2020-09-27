d27f8539ea944f5bec425c349177551ddd00f398
Shivam Kumar <shivam.kumar@gdn-commerce.com>
2020-09-27 21:51:16 +0530

    added ssl

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const fs = require('fs-extra')
 module.exports = {
   devServer: {
+    https: {
+      key: fs.readFileSync('ssl/server.key'),
+      cert: fs.readFileSync('ssl/server.crt')
+    },
     proxy: {
       '/api': {
         target: 'https://rickandmortyapi.com',
