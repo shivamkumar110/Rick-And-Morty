d6cd24b66eade6eafb121a2761ead5949e733b7a
Shivam Kumar <shivam.kumar@gdn-commerce.com>
2020-09-28 02:36:31 +0530

    added axios interceptor for dev and updated ssl keys

--- src/utils/callApi.js
+++ src/utils/callApi.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://rickandmortyapi.com'
+}
+
 export default {
   getRequest (path, configs) {
     return axios.get(path, configs)
